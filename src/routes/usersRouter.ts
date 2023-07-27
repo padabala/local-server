bfd3fb3b8231b689ad886a65a4fd6d3990da27eb
Phaneendra Adabala <phaneendra.adabala@qvik.fi>
2023-07-27 13:21:36 +0300

    renamed user data

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import * as users from "../data/users.json";
+import * as usersData from "../data/users.json";
 
 export const usersRouter = express.Router();
 
@@ -7,7 +7,7 @@ usersRouter.use(express.json());
 
 usersRouter.get("/users/:id", async (_req: Request, res: Response) => {
   try {
-    const user = users.users.filter((user) => user.id === _req.params.id);
+    const user = usersData.users.filter((user) => user.id === _req.params.id);
     res.status(200).send(user);
   } catch (error) {
     res.status(500).send(error);
