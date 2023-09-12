83cb96f008a7b50c15232508ffa85a22f8ecdbfd
Phaneendra Adabala <phaneendra.adabala@qvik.fi>
2023-09-12 15:16:37 +0300

    added new end points

--- src/routes/salesRouter.ts
+++ src/routes/salesRouter.ts
@@ -5,9 +5,26 @@ export const salesRouter = express.Router();
 
 salesRouter.use(express.json());
 
-salesRouter.get("/sales", async (_req: Request, res: Response) => {
+salesRouter.post("/sales/sync", async (_req: Request, res: Response) => {
   try {
     res.status(200).send(saleItems.saleItems);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+salesRouter.post("/sales/delete", async (_req: Request, res: Response) => {
+  try {
+    res.status(200).send({});
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+salesRouter.post("/sales/create", async (_req: Request, res: Response) => {
+  try {
+    console.log("create sale" + JSON.stringify(_req.body));
+    res.status(200).send(_req.body);
   } catch (error) {
     res.status(500).send(error);
   }
--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -13,3 +13,30 @@ usersRouter.get("/users/:id", async (_req: Request, res: Response) => {
     res.status(500).send(error);
   }
 });
+
+usersRouter.post("/users/register", async (_req: Request, res: Response) => {
+  try {
+    const user = usersData.users.filter((user) => user.id === _req.params.id);
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+usersRouter.post("/users/login", async (_req: Request, res: Response) => {
+  try {
+    console.log("/users/login" + JSON.stringify(_req.body));
+    console.log("username" + JSON.stringify(_req.body.username));
+    console.log("password" + JSON.stringify(_req.body.password));
+    const username = _req.body.username;
+    const password = _req.body.password;
+    const users = usersData.users.filter((user) => user.email === username);
+    if (users.length > 0) {
+      res.status(200).send(users[0]);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
